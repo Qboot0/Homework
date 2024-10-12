1722bec5c4863c7291059c4852d958c4b92521d6
Qboot0 <mambaitali@vk.com>
2024-10-12 14:22:42 +0300

    Add files via upload
    домашнее задание по теме «Организация программ и методы строк»

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+from dataclasses import replace
+from time import perf_counter
+
+my_string = input("введите текст:")
+print(len(my_string ))
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(" ",  ""))
+print(my_string [0])
+print(my_string [-1])
