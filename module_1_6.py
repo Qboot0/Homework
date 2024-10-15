5ceb8dd4a0cb79878e5e5c2ea7333edc2966a999
Qboot0 <mambaitali@vk.com>
2024-10-15 17:35:14 +0300

    Практическое задание по теме: "Словари и множества"
    Практическое задание по теме: "Словари и множества"

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {"Dima": 2004 , "Diana":2011 , "Max": 2020}
+print(my_dict)
+print(my_dict ["Dima"])
+print(my_dict.get ("Sahsa"))
+my_dict.update({"Dasha":2000, "Masha":2001 })
+del my_dict["Max"]
+print(my_dict)
+my_set = {1,1,1,2,2,3,4,5,5,4,4,6,7}
+print(my_set)
+my_set.add (8)
+my_set.add (9)
+my_set.discard(3)
+print(my_set)
