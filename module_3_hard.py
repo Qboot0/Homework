b82080d120e89931f32e17bb440d1c2b6826874c
Qboot0 <mambaitali@vk.com>
2024-11-26 21:35:09 +0300

    module 3 hard

---
+++ module_3_hard.py
@@ -0,0 +1,27 @@
+def calculate_structure_sum(data_structure):
+    total_sum = 0
+    for element in data_structure:
+        if isinstance(element, list):
+            total_sum += calculate_structure_sum(element)
+        elif isinstance(element, dict):
+            total_sum += calculate_structure_sum(list(element.values())) + sum(len(key) for key in element.keys())
+        elif isinstance(element, tuple):
+            total_sum += calculate_structure_sum(list(element))
+        elif isinstance(element, str):
+            total_sum += len(element)
+        elif isinstance(element, int):
+            total_sum += element
+    return total_sum
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
+
+
