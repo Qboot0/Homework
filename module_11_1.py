6ae08dd37557fb02840bde1b6c254a157d7cd69e
Qboot0 <mambaitali@vk.com>
2025-01-15 17:23:11 +0300

    Домашнее задание по теме "Обзор сторонних библиотек Python"

---
+++ module_11_1.py
@@ -0,0 +1,28 @@
+#pandas
+import pandas as pd
+
+data = pd.read_csv('data.csv')
+
+print("Имена столбцов:", data.columns)
+
+average_value = data['Column3'].mean()
+
+print("Среднее значение Column3:", average_value)
+
+#matplotlib
+import matplotlib
+matplotlib.use('Agg')  # Используйте бэкенд без GUI
+
+import matplotlib.pyplot as plt
+
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+
+plt.plot(x, y, marker='o')
+plt.title('Пример линейного графика')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.grid()
+plt.savefig('plot.png')  # Сохранение графика
